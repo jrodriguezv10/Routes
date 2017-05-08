6e8424b18aca074f58090b9fdf707220789eefb2
AlvarDev <deivid07.21@gmail.com>
2017-05-08 20:09:23 -0300

    init routes

---
+++ js/caller-routes.js
@@ -0,0 +1,15 @@
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize() {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: new google.maps.LatLng(-25.383948, -49.246980),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        styles: mapStylesGray
+    }
+
+    map = new google.maps.Map(mapCanvas, mapOptions);
+    console.log("initialized");
+}
